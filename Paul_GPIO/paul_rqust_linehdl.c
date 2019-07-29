476a495cad299defef3bd89a5907cebd01a68ee1
paul5566 <eric20021232000@gmail.com>
2019-07-29 07:35:58 -0700

    Add the Code to implement Kernel GPIO New Feature
    Add code:
    (1)Kernel_GPIO_Tool/gpio-utils.c
    	Add code as the reference
    (2)Kernel_GPIO_Tool/gpio-utils.h
    	Add code as the reference
    (3)Paul_GPIO/paul_rqust_linehdl.c
            Implement the baisc rqust with multipl to target chip
    (4)Paul_GPIO/gpio-utils.c
    
    Note:
    Add compile FLAG:-D_GNU_SOURCE
    That's for function "asprintf"
    	e.g:gcc -Wall -D_GNU_SOURCE -o

--- Paul_GPIO/paul_rqust_linehdl.c
+++ Paul_GPIO/paul_rqust_linehdl.c
@@ -1,31 +1,35 @@
 //asprintf funxtion
 #define _GNU_SOURCE
 
-
-#include <linux/gpio.h>
-#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <errno.h>
 #include <string.h>
+#include <poll.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <sys/ioctl.h>
+#include <linux/gpio.h>
+#include "gpio-utils.h"
 #include <asm/types.h>
 #include <termios.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <linux/gpio.h>
 #include <sys/fcntl.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
 #include <errno.h>
 
 //define GPIO device
-#define GPIO0 "/dev/gpiochip0"
-#define GPIO1 "/dev/gpiochip1"
-#define GPIO2 "/dev/gpiochip2"
-#define GPIO3 "/dev/gpiochip3"
-#define GPIO4 "/dev/gpiochip4"
-#define GPIO5 "/dev/gpiochip5"
+#define GPIO0 "gpiochip0"
+#define GPIO1 "gpiochip1"
+#define GPIO2 "gpiochip2"
+#define GPIO3 "gpiochip3"
+#define GPIO4 "gpiochip4"
+#define GPIO5 "gpiochip5"
 /*define COMSUMER*/
-#define COMSUMER "gpio-utils"
+#define COMSUMER "PAUL_TEST"
 
 enum direction {
 	OUT = 0,
@@ -45,7 +49,8 @@ int gpiotools_request_linehandle(const char *device_name, unsigned int *lines,
 	int i;
 	int ret;
 
-	ret = asprintf(&chrdev_name, "%s", device_name);
+
+	ret = asprintf(&chrdev_name, "/dev/%s", device_name);
 	if (ret < 0)
 		return -ENOMEM;
 
@@ -56,57 +61,89 @@ int gpiotools_request_linehandle(const char *device_name, unsigned int *lines,
 		goto exit_close_error;
 	}
 
-	for (i = 0; i < nlines; i++)
+	for (i = 0; i < nlines; i++)	{
 		req.lineoffsets[i] = lines[i];
-
+		printf("req.lineoffsets[%d] is %d\n",i,lines[i]);
+	}
 	req.flags = flag;
+	printf("flag is %d\n");
 	strcpy(req.consumer_label, consumer_label);
 	req.lines = nlines;
-	if (flag & GPIOHANDLE_REQUEST_OUTPUT)
+	if (flag & GPIOHANDLE_REQUEST_OUTPUT){
 		memcpy(req.default_values, data, sizeof(req.default_values));
-
+		printf("memcopy sucessdully\n");
+		printf("the req.default_values is %hhn\n",req.default_values);
+	}
+	printf("***executed the IOCTL in fuction:%s***\n",__func__);
 	ret = ioctl(fd, GPIO_GET_LINEHANDLE_IOCTL, &req);
 	if (ret == -1) {
 		ret = -errno;
 		fprintf(stderr, "Failed to issue GET LINEHANDLE IOCTL (%d)\n",
 			ret);
 	}
-
 exit_close_error:
 	if (close(fd) == -1)
 		perror("Failed to close GPIO character device file");
 	free(chrdev_name);
+
 	return ret < 0 ? ret : req.fd;
+}
+
+/**
+ * gpiotools_release_linehandle(): Release the line(s) of gpiochip
+ * @fd:			The fd returned by
+ *			gpiotools_request_linehandle().
+ *
+ * Return:		On success return 0;
+ *			On failure return the errno.
+ */
+int gpiotools_release_linehandle(const int fd)
+{
+	int ret;
+
+	ret = close(fd);
+	if (ret == -1) {
+		perror("Failed to close GPIO LINEHANDLE device file");
+		ret = -errno;
+	}
+	return ret;
 }
 
 
 
+
 int main(void){
-	//int val_gpiotools_gets;
 
-
-
-	//struct gpiochip_info chip_info;
-	//struct gpioline_info line_info;
 	struct gpiohandle_data data;
-	unsigned int lines[] = {0};
+	/*lines stored the line of the chip to request*/
+	/*program a code to detect*/
+	unsigned int lines[32] = {15,16};
 	int fd;
-	int debugfs_dir = IN;
-	int debugfs_value = 0;
-	int ret;
-
-	// Call gpiotools_request_linehandle()
-	ret = gpiotools_request_linehandle(GPIO0,  lines, 1, \
-			GPIOHANDLE_REQUEST_OUTPUT,&data,COMSUMER);
-	if (ret < 0)
-		goto fail_out;
-	else
-		fd = ret;
-	printf("the gpiotools_request_linehandle return value is %d\n",ret);
-	close(fd);
-	return 0;
-fail_out:
-	if (ret)
-		printf("FAILED\n");
+	int rv;
+	//gpiochip target:
+	char *targetchip = "gpiochip2";
+	memset(&data.values, 0, sizeof(data.values));
+	//gpiotools_requeshandle("chip name,line of the chip to request...")
+	rv = gpiotools_request_linehandle(targetchip, lines, 2,
+				GPIOHANDLE_REQUEST_OUTPUT,&data,COMSUMER);
+	if (rv < 0){
+		printf("REQUEST LINEHANDLE FAILED\n");
+		close(fd);
+	}
+	else{
+		fd = rv;
+		printf("REQUEST LINEHANDLE SUCESSFULLY\n");
+	}
+	system("./lsgpio");
+	sleep(15);
+	//gpiotools_release_linehandle
+	rv = gpiotools_release_linehandle(fd);
+	//error handling of release_linehandle
+	if(rv == 0)
+		printf("RELEASE LINEHANDLE SUCESSFULLY\n");
+	else {
+		printf("Failed to Release Linehandle of the %s with errno %d\n",targetchip,rv);
+		close(fd);
+	}
 	return 0;
 }
