e88fd9523cb14c9cef5361d0a09930d85d56bce0
paul5566 <eric20021232000@gmail.com>
2019-07-30 06:51:21 -0700

    Add the Code to implement the gpiotolls_get_value
    Add Code:
    
    (1)get_gpio_chip_info
    Get the line and chip info to limited the
    	(a)function:get_chip_info
    	(b)function:get line_info
    
    (2)gpio-hmmer.c
    Add as the reference
    
    (3)paul_rqust_linehdl
    	(a)unsigned int lines[32] = {15};
    		set control line ingpiochip only one line
    
    	(b)passed only 1 parameter to gpiotools_request_linehandle
    		Follow the program setting for control one chip's
    		line evrytime
    	(c)Add gpio tool get value function && error handling
    		Error Handling: 0 -> sucessfully
    				other ->errno number
    
    Remove Code:
    
    (1)deleted the name_check.c
    useless code
    
    (2)Modified paul_rqust_linehdl.c
    	(a)Remove some unuseful printf function
    	(b)Remove the redefiniton of "_GNU_SOURCE"
    	(c)Remove the close(fd) for redundant part
    		We have uesed function(fd )it the request_line_handle
    		exit error handling.

--- Project_Practice/gpio-hammer.c
+++ Project_Practice/gpio-hammer.c
@@ -1,5 +1,15 @@
-//asprintf function
-//#define _GNU_SOURCE
+/*
+ * gpio-hammer - example swiss army knife to shake GPIO lines on a system
+ *
+ * Copyright (C) 2016 Linus Walleij
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * Usage:
+ *	gpio-hammer -n <device-name> -o <offset1> -o <offset2>
+ */
 
 #include <unistd.h>
 #include <stdlib.h>
@@ -14,29 +24,20 @@
 #include <sys/ioctl.h>
 #include <linux/gpio.h>
 #include "gpio-utils.h"
-#include <asm/types.h>
-#include <termios.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/fcntl.h>
-#include <errno.h>
 
-//define GPIO device
-#define GPIO0 "gpiochip0"
-#define GPIO1 "gpiochip1"
-#define GPIO2 "gpiochip2"
-#define GPIO3 "gpiochip3"
-#define GPIO4 "gpiochip4"
-#define GPIO5 "gpiochip5"
-/*define COMSUMER*/
-#define COMSUMER "PAUL_TEST"
-
-enum direction {
-	OUT = 0,
-	IN = 1
-};
-
-
+/**
+ * doc: Operation of gpio
+ *
+ * Provide the api of gpiochip for chardev interface. There are two
+ * types of api.  The first one provide as same function as each
+ * ioctl, including request and release for lines of gpio, read/write
+ * the value of gpio. If the user want to do lots of read and write of
+ * lines of gpio, user should use this type of api.
+ *
+ * The second one provide the easy to use api for user. Each of the
+ * following api will request gpio lines, do the operation and then
+ * release these lines.
+ */
 /**
  * gpiotools_request_linehandle() - request gpio lines in a gpiochip
  * @device_name:	The name of gpiochip without prefix "/dev/",
@@ -60,7 +61,6 @@ enum direction {
  * Return:		On success return the fd;
  *			On failure return the errno.
  */
-
 int gpiotools_request_linehandle(const char *device_name, unsigned int *lines,
 				 unsigned int nlines, unsigned int flag,
 				 struct gpiohandle_data *data,
@@ -71,7 +71,6 @@ int gpiotools_request_linehandle(const char *device_name, unsigned int *lines,
 	int fd;
 	int i;
 	int ret;
-
 
 	ret = asprintf(&chrdev_name, "/dev/%s", device_name);
 	if (ret < 0)
@@ -84,30 +83,26 @@ int gpiotools_request_linehandle(const char *device_name, unsigned int *lines,
 		goto exit_close_error;
 	}
 
-	for (i = 0; i < nlines; i++)	{
+	for (i = 0; i < nlines; i++)
 		req.lineoffsets[i] = lines[i];
-		printf("req.lineoffsets[%d] is %d\n",i,lines[i]);
-	}
+
 	req.flags = flag;
 	strcpy(req.consumer_label, consumer_label);
 	req.lines = nlines;
-	if (flag & GPIOHANDLE_REQUEST_OUTPUT){
+	if (flag & GPIOHANDLE_REQUEST_OUTPUT)
 		memcpy(req.default_values, data, sizeof(req.default_values));
-		//printf("memcopy sucessdully\n");
-		//printf("the req.default_values is %hhn\n",req.default_values);
-	}
-	//printf("***executed the IOCTL in fuction:%s***\n",__func__);
+
 	ret = ioctl(fd, GPIO_GET_LINEHANDLE_IOCTL, &req);
 	if (ret == -1) {
 		ret = -errno;
 		fprintf(stderr, "Failed to issue GET LINEHANDLE IOCTL (%d)\n",
 			ret);
 	}
+
 exit_close_error:
 	if (close(fd) == -1)
 		perror("Failed to close GPIO character device file");
 	free(chrdev_name);
-
 	return ret < 0 ? ret : req.fd;
 }
 
@@ -120,7 +115,6 @@ exit_close_error:
  * Return:		On success return 0;
  *			On failure return the errno.
  */
-
 int gpiotools_set_values(const int fd, struct gpiohandle_data *data)
 {
 	int ret;
@@ -134,7 +128,6 @@ int gpiotools_set_values(const int fd, struct gpiohandle_data *data)
 
 	return ret;
 }
-
 /**
  * gpiotools_get_values(): Get the value of gpio(s)
  * @fd:			The fd returned by
@@ -144,7 +137,6 @@ int gpiotools_set_values(const int fd, struct gpiohandle_data *data)
  * Return:		On success return 0;
  *			On failure return the errno.
  */
-
 int gpiotools_get_values(const int fd, struct gpiohandle_data *data)
 {
 	int ret;
@@ -158,7 +150,6 @@ int gpiotools_get_values(const int fd, struct gpiohandle_data *data)
 
 	return ret;
 }
-
 /**
  * gpiotools_release_linehandle(): Release the line(s) of gpiochip
  * @fd:			The fd returned by
@@ -176,56 +167,137 @@ int gpiotools_release_linehandle(const int fd)
 		perror("Failed to close GPIO LINEHANDLE device file");
 		ret = -errno;
 	}
+
 	return ret;
 }
 
 
-
-
-int main(void){
-
+int hammer_device(const char *device_name, unsigned int *lines, int nlines,
+		  unsigned int loops)
+{
 	struct gpiohandle_data data;
-	/*lines stored the line of the chip to request*/
-	unsigned int lines[32] = {15};
+	char swirr[] = "-\\|/";
 	int fd;
-	int rv;
-	//gpiochip target:
-	char *targetchip = "gpiochip2";
-	memset(&data.values, 0, sizeof(data.values));
-	//gpiotools_requeshandle("chip name,line of the chip to request...")
-	rv = gpiotools_request_linehandle(targetchip, lines, 1,
-				GPIOHANDLE_REQUEST_OUTPUT,&data,COMSUMER);
-	if (rv < 0){
-		printf("REQUEST LINEHANDLE FAILED\n");
-		//close(fd); it close in the gpiotools_request_linehandle
-	}
-	else{
-		fd = rv;
-		printf("REQUEST LINEHANDLE SUCESSFULLY\n");
-	}
-	/*Using lsgpio to check*/
-	system("./lsgpio");
-	//sleep(15);
+	int ret;
+	int i, j;
+	unsigned int iteration = 0;
 
-	//get value
-	rv = gpiotools_get_values(fd, &data);
-	//error handling of gpiotools_get_values
-	if(rv == 0){
-		printf("GET VALUES SUCESSFULLY\n");
-		fd = rv;
+	memset(&data.values, 0, sizeof(data.values));
+	ret = gpiotools_request_linehandle(device_name, lines, nlines,
+					   GPIOHANDLE_REQUEST_OUTPUT, &data,
+					   "gpio-hammer");
+	if (ret < 0)
+		goto exit_error;
+	else
+		fd = ret;
+
+	ret = gpiotools_get_values(fd, &data);
+	if (ret < 0)
+		goto exit_close_error;
+
+	fprintf(stdout, "Hammer lines [");
+	for (i = 0; i < nlines; i++) {
+		fprintf(stdout, "%d", lines[i]);
+		if (i != (nlines - 1))
+			fprintf(stdout, ", ");
 	}
-	else {
-		printf("Failed to GET VALYES to %s with errno %d\n",targetchip,rv);
-		close(fd);
+	fprintf(stdout, "] on %s, initial states: [", device_name);
+	for (i = 0; i < nlines; i++) {
+		fprintf(stdout, "%d", data.values[i]);
+		if (i != (nlines - 1))
+			fprintf(stdout, ", ");
 	}
-	//gpiotools_release_linehandle
-	rv = gpiotools_release_linehandle(fd);
-	//error handling of release_linehandle
-	if(rv == 0)
-		printf("RELEASE LINEHANDLE SUCESSFULLY\n");
-	else {
-		printf("Failed to Release Linehandle of the %s with errno %d\n",targetchip,rv);
-		close(fd);
+	fprintf(stdout, "]\n");
+
+	/* Hammertime! */
+	j = 0;
+	while (1) {
+		/* Invert all lines so we blink */
+		for (i = 0; i < nlines; i++)
+			data.values[i] = !data.values[i];
+
+		ret = gpiotools_set_values(fd, &data);
+		if (ret < 0)
+			goto exit_close_error;
+
+		/* Re-read values to get status */
+		ret = gpiotools_get_values(fd, &data);
+		if (ret < 0)
+			goto exit_close_error;
+
+		fprintf(stdout, "[%c] ", swirr[j]);
+		j++;
+		if (j == sizeof(swirr)-1)
+			j = 0;
+
+		fprintf(stdout, "[");
+		for (i = 0; i < nlines; i++) {
+			fprintf(stdout, "%d: %d", lines[i], data.values[i]);
+			if (i != (nlines - 1))
+				fprintf(stdout, ", ");
+		}
+		fprintf(stdout, "]\r");
+		fflush(stdout);
+		sleep(1);
+		iteration++;
+		if (loops && iteration == loops)
+			break;
 	}
-	return 0;
+	fprintf(stdout, "\n");
+	ret = 0;
+
+exit_close_error:
+	gpiotools_release_linehandle(fd);
+exit_error:
+	return ret;
+}
+
+void print_usage(void)
+{
+	fprintf(stderr, "Usage: gpio-hammer [options]...\n"
+		"Hammer GPIO lines, 0->1->0->1...\n"
+		"  -n <name>  Hammer GPIOs on a named device (must be stated)\n"
+		"  -o <n>     Offset[s] to hammer, at least one, several can be stated\n"
+		" [-c <n>]    Do <n> loops (optional, infinite loop if not stated)\n"
+		"  -?         This helptext\n"
+		"\n"
+		"Example:\n"
+		"gpio-hammer -n gpiochip0 -o 4\n"
+	);
+}
+
+int main(int argc, char **argv)
+{
+	const char *device_name = NULL;
+	unsigned int lines[GPIOHANDLES_MAX];
+	unsigned int loops = 0;
+	int nlines;
+	int c;
+	int i;
+
+	i = 0;
+	while ((c = getopt(argc, argv, "c:n:o:?")) != -1) {
+		switch (c) {
+		case 'c':
+			loops = strtoul(optarg, NULL, 10);
+			break;
+		case 'n':
+			device_name = optarg;
+			break;
+		case 'o':
+			lines[i] = strtoul(optarg, NULL, 10);
+			i++;
+			break;
+		case '?':
+			print_usage();
+			return -1;
+		}
+	}
+	nlines = i;
+
+	if (!device_name || !nlines) {
+		print_usage();
+		return -1;
+	}
+	return hammer_device(device_name, lines, nlines, loops);
 }
