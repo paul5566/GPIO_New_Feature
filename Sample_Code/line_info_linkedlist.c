975e01edbe75138373c541571db03627d7515ac1
paul5566 <eric20021232000@gmail.com>
2019-08-01 19:14:52 -0700

    Add code in line_info_linkedlist.c to store line
    Add Code:
    
    (1)"line_info_linkedlist.c"
    Stored Data in  line and chip info to limited the
            (a)find_node function:
    		find the node to store gpio
            (b)fill_line_info function:
    		fill the line info as the Dummy Node for testing
    	(c)chip_line_check
    		check the target chip and line is used or unused
    	(d)find node:
    		find the node which stored in gpiochip
    	(e)print all:
    		print all the node's gpiochip_no and gpio_line
    		status. Used/Unused
    
    Note:
    This is the sample code to make the parser to find chip and store line.
    Besieds, The code would check the targert(gpiochip's line).The program
    would check the specfic line: used/unused

--- Sample_Code/line_info_linkedlist.c
+++ Sample_Code/line_info_linkedlist.c
@@ -10,7 +10,7 @@ struct node {
 	int gpio_line[32];
 };
 
-struct node *find_tail(struct node *head){
+static struct node *find_tail(struct node *head){
 	struct node *current;
 
 	current = head;
@@ -20,7 +20,16 @@ struct node *find_tail(struct node *head){
 	}
 	return current;
 }
+static struct node *find_node(struct  node *head, int target){
+	struct node *current;
 
+	current = head;
+	while(current->gpio_chip_no != target)
+	{
+		current = current->next;
+	}
+	return current;
+}
 
 /*
 	create new node
@@ -28,7 +37,7 @@ struct node *find_tail(struct node *head){
 	*****Be careful*****
 	Create Node Error Handling
 */
-void create_new_node(struct node *head,int val){
+static void create_new_node(struct node *head,int val){
 
 	struct node *new_node;
     struct node *current_tail;
@@ -43,17 +52,51 @@ void create_new_node(struct node *head,int val){
     current_tail->next = new_node;
     new_node->next = NULL;
 }
+/*
+ *	Assign data to node
+ *	Tell the flag "unused" to fill
+ *	line info
+ */
 
+static void fill_line_info(struct node *target){
+	//Add the flag check later
+	for(int i = 0; i < 32; i++)
+	{
+		target->gpio_line[i] = 1;
+	}
+}
+/*
+ *
+ */
 
-void print_all(struct node *head){
+static void chip_line_check(struct node *head,int chip_no,int chip_line)
+{
+	struct node *assign_chip_info;
+
+	assign_chip_info = find_node(head,chip_no);
+	if(assign_chip_info->gpio_line[chip_line])
+		printf("the gpiochip%d's %dnd line is used\n",chip_no, chip_line);
+	else
+		printf("the gpiochip%d's %dne line is unused\n",chip_no, chip_line);
+}
+
+/*
+ * print all the node's line status
+ */
+static void print_all(struct node *head){
 	struct node *current;
 	current = head;
-	int i = 1;
-	while(current != NULL)//current->value != NULL the last point would not print out
+	int i = 0;
+	while(current->next != NULL)//current->value != NULL the last point would not print out
 	{
-		printf("the %d of the chip_number is %d\n",i, current->gpio_chip_no);
+		printf("\n***the chip_number is %d***\n", current->gpio_chip_no);
 		current = current->next;
 		i++;
+		for(int j = 0;j < 32;j++)
+		{
+			printf("line_info[%d] = %d\n", j, current->gpio_line[j]);
+		}
+
 	}
 }
 
@@ -64,6 +107,8 @@ int main(void)
 	   Build up the head node
 	*/
 	struct node *head;//declare a pointer point to the type struct
+	struct node *assign_chip_info;
+
 	head = malloc(sizeof(struct node));//memory allocate a segment
 
 	if (head == NULL)
@@ -71,7 +116,7 @@ int main(void)
 		printf("the memory allocation failed\n");
 	}
 
-	head->gpio_chip_no = 0;//Set gpiochip0 as the first chip
+	head->gpio_chip_no = 4;//Set gpiochip0 as the first chip
 	head->next = NULL;//assign the NULL to node head's *next which means the next would be NULL;
 	/*
 	   Build up the tail node
@@ -83,12 +128,23 @@ int main(void)
 		printf("the memory allocation failed\n");
 	}
 
-	tail->gpio_chip_no = 1;
+	tail->gpio_chip_no = 3;
 	head->next = tail;//create the link between the head and tail
 	tail->next = NULL;
 	create_new_node(head,2);
-	create_new_node(head,3);
-	create_new_node(head,4);
-	print_all(head);
+	create_new_node(head,1);
+	create_new_node(head,0);
+	//find node
+	assign_chip_info = find_node(head,3);
+	//assign the line_info
+	fill_line_info(assign_chip_info);
+	//print all
+	//print_all(head);
+	//test line
+	int gpiochip_test = 3;
+	int test_line = 16;
+	//check function
+	chip_line_check(head,gpiochip_test,test_line);
+
 	return 0;
 }
